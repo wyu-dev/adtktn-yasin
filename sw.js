3e26d3c649391ab1c251fdc583d30ea9ffde9b5f
WAHYU YUSOF <wahyuddin.yusof@gmail.com>
2026-03-08 10:20:23 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,32 @@
-const CACHE = 'yasin-adtec-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'yasin-adtec-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/index.html')))
+    caches.match(e.request).then(cached => {
+      return cached || fetch(e.request).catch(() => caches.match('./index.html'));
+    })
   );
 });
